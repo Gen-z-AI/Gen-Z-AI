22a0ea8d9ea204332a4c364e048ccb90b46f3840
Rajanvik <rajankumar15052002@gmaail.com>
2024-04-11 20:07:05 +0530

    Folder Structure

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <div className="text-center item-center mt-20">
+      <h1 className="text-5xl font-bold">
+        Gen'z-AI
+      </h1>
+      <h1 className="text-5xl font-bold">
+        How can I help you today?
+      </h1>
+    </div>
+  )
+}
+
+export default App
