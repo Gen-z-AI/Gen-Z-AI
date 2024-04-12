45aee2b813b9ff82b7cb559e0951dbeaff88cd1b
Rajanvik <rajankumar15052002@gmaail.com>
2024-04-12 23:50:31 +0530

    Shadcn Installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import Auth0UserInfo from "./component/auth0UserInfo/Auth0UserInfo"
+import Auth0UserInfo from "./components/auth0UserInfo/Auth0UserInfo"
+import { Button } from "@/components/ui/button"
 
 function App() {
 
@@ -11,7 +12,10 @@ function App() {
       <h1 className="text-5xl font-bold">
         How can I help you today?
       </h1>
-      <Auth0UserInfo/>
+      <div className="item-center">
+        <Auth0UserInfo/>
+      </div>
+      <Button>Click me</Button>
     </div>
   )
 }
