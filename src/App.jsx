ee903b999ea669af12e6087c4e8e85c22d29a251
Druti Bose <113259277+coderDruti@users.noreply.github.com>
2024-04-13 16:55:01 +0530

    Update App.jsx
    Please update the App.jsx according to the code written in the comments

--- src/App.jsx
+++ src/App.jsx
@@ -23,3 +23,19 @@ function App() {
 }
 
 export default App
+// import Main from "./component/main/Main.jsx";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <>
+//       {/* <div className="text-center item-center mt-20">
+//         <h1 className="text-5xl font-bold">Gen-z-AI</h1>
+//         <h1 className="text-5xl font-bold">How can I help you today?</h1>
+//       </div> */}
+//       <Main />
+//     </>
+//   );
+// }
+
+// export default App;
