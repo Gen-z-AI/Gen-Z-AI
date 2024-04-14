e8a821080e49c6159345462169da6dbcb0de1118
Rajanvik <rajankumar15052002@gmaail.com>
2024-04-14 23:30:33 +0530

    app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Navbar from './components/navbar/Navbar'
 import AnimatedCursor from "react-animated-cursor"
@@ -6,13 +5,6 @@ import Hero from "./components/hero/Hero";
 import Footer from "./components/footer/Footer";
 import ServicesList from "./components/services/ServicesList";
 import Team from "./components/team/Team";
-=======
-import React from "react"
-import Auth0UserInfo from "./components/auth0UserInfo/Auth0UserInfo"
-import { Button } from "@/components/ui/button"
-import Main from "./components/main/Main.jsx";
-import "./App.css";
->>>>>>> 68122cb301b9f55037079abc48f1cf11be7e1232
 
 function App() {
   return (
@@ -40,35 +32,8 @@ function App() {
         <Team/>
         <Footer/>
       </div>
-<<<<<<< HEAD
     </>
   )
 }
 
 export default App;
-=======
-
-      {/* shadcn componenet */}
-      <Button>Click me</Button>
-      <Main />
-    </div>
-  )
-}
-
-export default App
-
-
-// function App() {
-//   return (
-//     <>
-//       {/* <div className="text-center item-center mt-20">
-//         <h1 className="text-5xl font-bold">Gen-z-AI</h1>
-//         <h1 className="text-5xl font-bold">How can I help you today?</h1>
-//       </div> */}
-
-//     </>
-//   );
-// }
-
-// export default App;
->>>>>>> 68122cb301b9f55037079abc48f1cf11be7e1232
