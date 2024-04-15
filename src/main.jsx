140c53f525d030a5154b96aaedc701bd8d088cfc
Druti Bose <113259277+coderDruti@users.noreply.github.com>
2024-04-15 13:56:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -16,8 +16,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Auth0Provider>
   </React.StrictMode>
 );
-// import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
-// import "./index.css";
 
-// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
