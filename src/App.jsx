9a6061ab742fdecd9ac70347a58db60a99c878ee
Druti Bose <113259277+coderDruti@users.noreply.github.com>
2024-04-16 04:49:39 +0000

    Modification2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Main from "./component/main/Main.jsx";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,11 @@ function App() {
         <h1 className="text-5xl font-bold">Gen-z-AI</h1>
         <h1 className="text-5xl font-bold">How can I help you today?</h1>
       </div> */}
+      <Auth0Provider
+        domain="dev-z1lfyi0hy3zoamlj.us.auth0.com"
+        clientId="OQJCOy2admqtvz1ynoVMOE3fRlDiwc23"
+        redirectUri={window.location.origin} // Use redirectUri instead of authorizationParams
+      ></Auth0Provider>
       <Main />
     </>
   );
